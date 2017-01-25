18c043e1723e05f241071faadb35230cc3f90255
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-24 17:43:52 -0800

    Completed student creation form.

--- hackbright-web.py
+++ hackbright-web.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 
 import hackbright
 
@@ -23,6 +23,31 @@ def get_student_form():
     """Show form for searching for a student"""
 
     return render_template("student_search.html")
+
+
+# @app.route("/student-add")
+# def get_add_student_form():
+#     """Show form to add student to database."""
+
+#     return render_template("student_add.html")
+
+
+@app.route("/student-add", methods=['GET', 'POST'])
+def add_student():
+    """Show form to add student, and then add student."""
+
+    if request.method == 'GET':
+        return render_template("student_add.html")
+    else:
+        first_name = request.form.get("firstname")
+        last_name = request.form.get("lastname")
+        github = request.form.get("github")
+        hackbright.make_new_student(first_name, last_name, github)
+        return render_template("student_add_success.html",
+                               first_name=first_name,
+                               last_name=last_name,
+                               github=github)
+
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
